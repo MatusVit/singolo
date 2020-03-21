c7bd91043771a7a353e65fb94a8821c77dbbee67
Vitali Matusevich <vimatu@gmail.com>
2020-03-21 19:14:10 +0300

    feat: add SliderControlClickHandler - all not implemented

--- script.js
+++ script.js
@@ -4,12 +4,15 @@ window.onload = function(){
     
     //header-nav
     addHeaderNavClickHandler();
-    
+
+    //Slider  
+    addSliderControlClickHandler();
+     
 }
 
 
 const addHeaderNavClickHandler = () => {
-    document.querySelector('.header-nav-list').addEventListener('click', (e) =>  {
+    document.querySelector('.header-nav-list').addEventListener('click', (e) => {
         if (e.target.classList.contains('nav-item')) {
             let clickedTag = e.target;     
             removeActiveTags('.header-nav-list .nav-item', 'nav-item--active');
@@ -27,4 +30,23 @@ const removeActiveTags = (selectorList, selectorTag) => {
 
 const addActiveTag = (clickedTag, selectorTag) => {
     clickedTag.classList.add(selectorTag);
+}
+
+
+
+ 
+
+const addSliderControlClickHandler = () => {
+    document.querySelector('.chev--left').addEventListener('click', (e) => {
+        let clickedTag = e.currentTarget;
+        // TODO function move left
+        console.log(clickedTag);
+        
+    });
+
+    document.querySelector('.chev--right').addEventListener('click', (e) => {
+        let clickedTag = e.currentTarget;
+        // TODO function move right
+        console.log(clickedTag);
+    });
 }
