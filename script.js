bc2bf81bb5f0402ba84700111cdd2519c95a3a8d
Vitali Matusevich <vimatu@gmail.com>
2020-03-22 18:40:22 +0300

    feat: implement Slider Control Click Handler

--- script.js
+++ script.js
@@ -58,16 +58,51 @@ const addActiveTag = (clickedTag, selectorTag) => {
 
 
 const addSliderControlClickHandler = () => {
-    document.querySelector('.chev--left').addEventListener('click', (e) => {
-        let clickedTag = e.currentTarget;
-        // TODO function move left
-        console.log(clickedTag);
+    const sliderScreens = document.querySelectorAll('.slider-screen');
+    let currentScreen = 0;
+    let isEnabled = true;
+    const changeCurrentScreens = (n) => {
+        currentScreen = (n + sliderScreens.length) % sliderScreens.length;
+    }
 
+    document.querySelector('.chev--left').addEventListener('click', (e) => {
+        if (isEnabled) {
+            slidePreviousScreen(currentScreen);
+        }
     });
 
     document.querySelector('.chev--right').addEventListener('click', (e) => {
-        let clickedTag = e.currentTarget;
-        // TODO function move right
-        console.log(clickedTag);
+        if (isEnabled) {
+            slideNextScreen(currentScreen);
+        }
     });
+
+    const slideNextScreen = (currentScreen) => {
+        hideScreen('to-left');
+        changeCurrentScreens(currentScreen + 1);
+        showScreen('from-right')
+    };
+
+    const slidePreviousScreen = (currentScreen) => {
+        hideScreen('to-right');
+        changeCurrentScreens(currentScreen - 1);
+        showScreen('from-left');
+    };
+
+    const hideScreen = (direction) => {
+        isEnabled = false;
+        sliderScreens[currentScreen].classList.add(direction);
+        sliderScreens[currentScreen].addEventListener('animationend', function() {
+            this.classList.remove('slider-active', direction);
+        });
+    }
+
+    const showScreen = (direction) => {
+        sliderScreens[currentScreen].classList.add('slider-next', direction);
+        sliderScreens[currentScreen].addEventListener('animationend', function() {
+            this.classList.remove('slider-next', direction);
+            this.classList.add('slider-active');
+            isEnabled = true;
+        });
+    }
 }
